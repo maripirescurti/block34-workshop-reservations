72b9f7bff16b6760377a8ca8c06ffb534a722032
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-10-06 22:38:06 -0400

    created tables

--- server/db.js
+++ server/db.js
@@ -1,6 +1,31 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_travel_db');
 
+const createTables = async() => {
+  const SQL = `
+    DROP TABLE IF EXISTS reservations;
+    DROP TABLE IF EXISTS customers;
+    DROP TABLE IF EXISTS restaurants;
+    CREATE TABLE customers(
+      id UUID PRIMARY KEY,
+      name VARCHAR(50) NOT NULL UNIQUE
+    );
+    CREATE TABLE restaurants(
+      id UUID PRIMARY KEY,
+      name VARCHAR(50) NOT NULL UNIQUE
+    );
+    CREATE TABLE reservations(
+      id UUID PRIMARY KEY,
+      date DATE NOT NULL,
+      party_count INTEGER NOT NULL,
+      restaurant_id UUID REFERENCES restaurants table NOT NULL,
+      customer_id UUID REFERENCES customer table NOT NULL
+    );
+  `;
+  await client.query(SQL);
+};
+
 module.exports = {
   client,
+  createTables
 };
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const { 
   client,
+  createTables,
 } = require('./db');
 
 const init = async() => {
   console.log('connecting to databse');
   await client.connect();
   console.log('connected to database');
+  await createTables();
+  console.log('created tables');
 };
 
 init ();
